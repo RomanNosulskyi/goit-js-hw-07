75d84c3303c93b83ed1485e2e7e2994c7cb75e05
RomanNosulskyi <nosulskyiroman@gmail.com>
2023-03-16 15:58:02 +0200

    js2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryEl = [];
+
+galleryItems.map((galleryItem) => {
+  const a = document.createElement("a");
+  a.classList.add("gallery__image");
+  a.href = galleryItem.original;
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = galleryItem.preview;
+  img.alt = galleryItem.description;
+
+  a.append(img);
+  galleryEl.push(a);
+});
+
+gallery.append(...galleryEl);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
